94877c671c957ed261e2c9e08255ec4f622d1c5d
Ajay Varma <kammampatiajayvarma@gmail.com>
2026-08-10 08:32:04 +0530

    wrote logic for getVideoInfo & getVideoOwnerInfo

---
+++ src/controllers/video.controllers.js
@@ -0,0 +1,68 @@
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHander } from "../utils/asyncHandler.js";
+import { Video } from "../models/video.models.js";
+import mongoose from "mongoose.js";
+
+const getVideoInfo=asyncHander(async (req,res)=>{
+    const {videoId}=req.body || req.params;
+
+    const video=await Video.findById(videoId);
+
+    if(!video){
+        throw new ApiError(404,"video doesn't exists with the video Id");
+    }
+
+    return res.status(200).json(new ApiResponse(200,video,"video info fetched successfully"));
+})
+
+const getVideoOwnerInfo=asyncHander(async (req,res)=>{
+    const {videoId}=req.body || req.params;
+
+    const video=await Video.aggregate([
+        {
+            $match:{
+                _id: new mongoose.Types.ObjectId(videoId)
+            }
+        },
+        {
+            $lookup:{
+                from:'users',
+                localField:'owner',
+                foreignField:'_id',
+                as:'videoOwner'              
+            }
+        },
+        {
+            $unwind:"$videoOwner"
+        },
+        {
+            $project:{ 
+                _id:1,
+                "videoOwner.username":1,
+                "videoOwner.email":1,
+                "videoOwner.fullName":1
+            }
+        }
+    ])
+
+    if(!video?.length){
+        throw new ApiError(404,"video owner info not found")
+    }
+
+    return res.status(200).json(new ApiResponse(200,video[0],"Video owner info fetched successfully"))
+})
+
+const getAllVideo=asyncHander(async (req,res)=>{
+    
+})
+
+const updateVideo=asyncHander(async (req,res)=>{
+
+})
+
+const deleteVideo=asyncHander(async (req,res)=>{
+
+})
+
+export {getVideoInfo,getVideoOwnerInfo};

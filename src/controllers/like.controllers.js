d93166bbe0ad76ede41d4cfb1900b01ef6dd14ff
Ajay Varma <kammampatiajayvarma@gmail.com>
2026-08-21 08:36:51 +0530

    wrote logic for toggling video's like, comment's like, tweet's like

---
+++ src/controllers/like.controllers.js
@@ -0,0 +1,80 @@
+import { Like } from "../models/like.models";
+import { ApiResponse } from "../utils/ApiResponse";
+import { asyncHander } from "../utils/asyncHandler";
+import { ApiError } from "../utils/ApiError";
+import mongoose,{isValidObjectId} from "mongoose";
+
+const toggleVideoLike=asyncHander(async(req,res)=>{
+    const {videoId}=req.params;
+
+    if(!isValidObjectId(videoId)){
+        throw new ApiError(400,"Invalid video ID");
+    }
+
+    const existingVideoLike=await Like.findOne({
+        video:videoId,
+        likedBy:req.user?._id
+    });
+
+    if(existingVideoLike){
+        await Like.findByIdAndDelete(existingVideoLike._id);
+        return res.status(200).json(new ApiResponse(200,{isLiked:false},"video unliked successfully"));
+    }
+
+    await Like.create({
+        video:videoId,
+        likedBy:req.user._id
+    });
+
+    return res.status(200).json(new ApiResponse(200,{isLiked:true},"video liked successfully"));
+})
+
+const toggleCommentLike=asyncHander(async(req,res)=>{
+    const {commentId}=req.params;
+
+    if(!isValidObjectId(commentId)){
+        throw new ApiError(400,"Invalid Comment ID");
+    }
+
+    const existingCommentLike=await Like.findOne({
+        comment:commentId,
+        likedBy:req.user._id
+    });
+
+    if(existingCommentLike){
+        await Like.findByIdAndDelete(existingCommentLike._id);
+        return res.status(200).json(new ApiResponse(200,{isLiked:false},"comment unliked successfully"));
+    }
+
+    await Like.create({
+        comment:commentId,
+        likedBy:req.user._id
+    });
+
+    return res.status(200).json(new ApiResponse(200,{isLiked:true},"comment liked successfully"));
+})
+
+const toggleTweetLike=asyncHander(async(req,res)=>{
+    const {tweetId}=req.params;
+
+    if(!isValidObjectId(tweetId)){
+        throw new ApiError(400,"Invalid Tweet ID");
+    }
+
+    const existingTweetLike=await Like.findOne({
+        tweet:tweetId,
+        likedBy:req.user._id
+    });
+
+    if(existingTweetLike){
+        await Like.findByIdAndDelete(existingTweetLike._id);
+        return res.status(200).json(new ApiResponse(200,{isLiked:false},"tweet unliked successfully"));
+    }
+
+    await Like.create({
+        tweet:tweetId,
+        likedBy:req.user._id
+    });
+
+    return res.status(200).json(new ApiResponse(200,{isLiked:true},"tweet liked successfully"));
+})
--- src/models/like.models.js
+++ src/models/like.models.js
@@ -2,7 +2,23 @@ import mongoose,{Schema} from "mongoose";
 
 const likeSchema=new Schema(
     {
-
+        video:{
+            type:Schema.Types.ObjectId,
+            ref:"Video"  
+        },
+        comment:{
+            type:Schema.Types.ObjectId,
+            ref:"Comment"
+        },
+        likedBy:{
+            type:Schema.Types.ObjectId,
+            ref:"Tweet"
+        },
+        tweet:{
+            type:Schema.Types.ObjectId,
+            ref:"User",
+            required: true
+        }
     },
     {
         timestamps:true

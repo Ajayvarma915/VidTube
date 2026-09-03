605c8e54994a581df077e0ebb5e2c86b2674ec38
Ajay Varma <kammampatiajayvarma@gmail.com>
2026-09-03 09:33:32 +0530

    wrote logic for comment controllers add video comment and add tweet comment

--- src/controllers/comment.controllers.js
+++ src/controllers/comment.controllers.js
@@ -3,11 +3,70 @@ import { ApiResponse } from "../utils/ApiResponse";
 import { asyncHandler } from "../utils/asyncHandler";
 import { Comment } from "../models/comment.models";
 
-const addComment=asyncHandler(async (req,res)=>{
-    
+const addVideoComment=asyncHandler(async (req,res)=>{
+    const {videoId}=req.params;
+    const {content}=req.body;
+
+    if(!content || content.trim==="") {
+        throw new ApiError(400,"Content is required");
+    }
+
+    if(!videoId){
+        throw new ApiError(400,"videoId is required to comment");
+    }
+
+    const comment=await Comment.create({
+        content:content.trim(),
+        video:videoId || null,
+        owner: req.user?._id
+    });
+
+    return res.status(201).json(new ApiResponse(201,comment,"video comment added successfully"));
+
 })
 
-const updateComment=asyncHandler(async (req,res)=>{
+const addTweetComment=asyncHandler(async (req,res)=>{
+    const {tweetId}=req.params;
+    const {content}=req.body;
+
+    if(!content || content.trim==="") {
+        throw new ApiError(400,"Content is required");
+    }
+
+    if(!tweetId){
+        throw new ApiError(400,"videoId is required to comment");
+    }
+
+    const comment=await Comment.create({
+        content:content.trim(),
+        tweet:tweetId || null,
+        owner: req.user?._id
+    });
+
+    return res.status(201).json(new ApiResponse(201,comment,"tweet comment added successfully"));
+
+})
+
+const updateVideoComment=asyncHandler(async (req,res)=>{
+    const {commentId}=req.params;
+    const {content}=req.body;
+
+    const comment=await Comment.findById(commentId);
+
+    if(!comment){
+        throw new ApiError(404,"Comment not found");
+    }
+
+    if(comment.owner.toString()!==req.user._id.toString()){
+        throw new ApiError(403,"you do not have permission to edit this comment");
+    }
+
+    const updatedComment=await Comment.findByIdAndUpdate({
+        
+    })
+})
+
+const updatetweetComment=asyncHandler(async (req,res)=>{
 
 })
 
@@ -24,4 +83,4 @@ const getTweetComments=asyncHandler(async (req,res)=>{
 })
 
 
-export {addComment,updateComment,deleteComment,getVideoComments,getTweetComments}
+export {addTweetComment,addVideoComment,updateVideoComment,updatetweetComment,deleteComment,getVideoComments,getTweetComments}

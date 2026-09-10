f3bcbfad7817fcfabd13cbcb3c35df97c5496ccf
Ajay Varma <kammampatiajayvarma@gmail.com>
2026-09-10 08:02:51 +0530

    created controllers for playlist

---
+++ src/controllers/playlist.controllers.js
@@ -0,0 +1,33 @@
+import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/ApiError";
+import { ApiResponse } from "../utils/ApiResponse";
+import { Playlist } from "../models/playlist.models";
+import mongoose,{isValidObjectId} from "mongoose";
+
+const createPlaylist=asyncHandler(async (req,res)=>{
+    
+})
+
+const updatePlaylist=asyncHandler(async (req,res)=>{
+
+})
+
+const deletePlaylist=asyncHandler(async (req,res)=>{
+
+})
+
+const removeVideoFromPlaylist=asyncHandler(async (req,res)=>{
+
+})
+
+const getUserPlaylists=asyncHandler(async (req,res)=>{
+
+})
+
+const addVideoToPlaylist=asyncHandler(async (req,res)=>{
+
+})
+
+const getPlaylistById=asyncHandler(async (req,res)=>{
+
+})
